b8ff317b509ee892506736637abcfe56aeefce96
fanyafeng <1181631922@qq.com>
2016-04-18 12:28:11 +0800

    recyclerview上拉加载下拉刷新

--- app/src/main/java/com/fanyafeng/orlitedemo/recycleview/activity/MyRecycleViewActivity.java
+++ app/src/main/java/com/fanyafeng/orlitedemo/recycleview/activity/MyRecycleViewActivity.java
@@ -4,17 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.fanyafeng.orlitedemo.BaseActivity;
 import com.fanyafeng.orlitedemo.R;
 import com.fanyafeng.orlitedemo.dao.Album;
 import com.fanyafeng.orlitedemo.dao.Song;
 import com.fanyafeng.orlitedemo.recycleview.adapter.MyRecycleViewAdapter;
+import com.orangegangsters.github.swipyrefreshlayout.library.SwipyRefreshLayout;
+import com.orangegangsters.github.swipyrefreshlayout.library.SwipyRefreshLayoutDirection;
 
 import org.litepal.crud.DataSupport;
 
@@ -25,6 +29,7 @@ public class MyRecycleViewActivity extends BaseActivity {
     private RecyclerView rv_myrecycleview;
     private List<String> stringList=new ArrayList<>();
     private MyRecycleViewAdapter myRecycleViewAdapter;
+    private SwipyRefreshLayout swipyrefreshlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +59,38 @@ public class MyRecycleViewActivity extends BaseActivity {
     }
 
     private void initView() {
+        swipyrefreshlayout= (SwipyRefreshLayout) findViewById(R.id.swipyrefreshlayout);
         rv_myrecycleview = (RecyclerView) findViewById(R.id.rv_myrecycleview);
     }
 
     private void initData() {
         myRecycleViewAdapter = new MyRecycleViewAdapter(this, stringList);
         rv_myrecycleview.setAdapter(myRecycleViewAdapter);
-        rv_myrecycleview.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+//        rv_myrecycleview.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        rv_myrecycleview.setLayoutManager(new GridLayoutManager(this,3,GridLayoutManager.VERTICAL,false));
+
+        swipyrefreshlayout.setOnRefreshListener(new SwipyRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh(SwipyRefreshLayoutDirection direction) {
+                if (direction==SwipyRefreshLayoutDirection.TOP){
+                    Toast.makeText(MyRecycleViewActivity.this,"下拉刷新",Toast.LENGTH_SHORT).show();
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    swipyrefreshlayout.setRefreshing(false);
+                }else if (direction==SwipyRefreshLayoutDirection.BOTTOM){
+                    Toast.makeText(MyRecycleViewActivity.this,"上拉加载",Toast.LENGTH_SHORT).show();
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    swipyrefreshlayout.setRefreshing(false);
+                }
+            }
+        });
     }
 
 
